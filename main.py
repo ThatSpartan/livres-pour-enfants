5e6418d9802da552dd42b3da7a97279e947d3dd3
ThatSpartan <dodo4931@outlook.com>
2018-05-12 19:50:23 -0400

    remade binary search (simple)

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-def BinarySearch(arr,left,right,val):
+def BinarySearch(arr, val):
 
-    if left > right:
+    if len(arr) == 0 or (len(arr) == 1 and arr[0] != val):
         return False
 
-    mid = (left + right) // 2
+    mid_count = len(arr) // 2
+    mid_val = arr[len(arr) // 2]
 
-    if arr[mid] == val:
+    print('Full Length : ' + str(len(arr)))
+    print('Mid Length : ' + str(len(arr) // 2))
+    print('Mid Length : ' + str(mid_count))
+    print('Mid Val : ' + str(mid_val))
+
+    if val == mid_val: # val is middle item
         return True
-    
-    if val < arr[mid]:
-        return BinarySearch(arr,left,mid-1,val)
-    else:
-        BinarySearch(arr,mid+1,right,val)
 
+    elif val < mid_val: # val is to the left (smaller)
+        print(arr[:mid_count])
+        return BinarySearch(arr[:mid_count], val)
+
+    elif val > mid_val: # val is to the right (bigger)
+        print(arr[mid_count+1:])        
+        return BinarySearch(arr[mid_count+1:], val)
+
+a = [1,2,3,4,5,6,7,8,9]
+
+print(BinarySearch(a, 3))
+print('---------')
+print(BinarySearch(a, 4))
+print('---------')
+print(BinarySearch(a, 5))
+print('---------')
+print(BinarySearch(a, 7))
