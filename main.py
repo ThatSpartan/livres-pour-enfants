c5032a391720e8f9b1d129605a12e8c72cc5beeb
ThatSpartan <dodo4931@outlook.com>
2018-05-12 20:02:18 -0400

    done

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ def BinarySearch(arr, val):
     mid_count = len(arr) // 2
     mid_val = arr[mid_count][0]
 
-    print('Full Length : ' + str(len(arr)))
-    print('Mid Length : ' + str(len(arr) // 2))
-    print('Mid Length : ' + str(mid_count))
-    print('Mid Val : ' + str(mid_val))
-
     if val == mid_val: # val is middle item
         return arr[mid_count][1]
 
@@ -21,13 +16,3 @@ def BinarySearch(arr, val):
     elif val > mid_val: # val is to the right (bigger)
         print(arr[mid_count+1:])        
         return BinarySearch(arr[mid_count+1:], val)
-
-a = [[1, 'one'],[2, 'two'],[3, 'three'],[4, 'four'],[5, 'five'],[6, 'six'],[7, 'seven'],[8, 'eight'],[9, 'nine']]
-
-print(BinarySearch(a, 3))
-print('---------')
-print(BinarySearch(a, 4))
-print('---------')
-print(BinarySearch(a, 5))
-print('---------')
-print(BinarySearch(a, 7))
